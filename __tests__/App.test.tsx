a4c1c28aa00157b2a21edc0d70bd234f9e4d9d0e
vgcman16 <155417613+vgcman16@users.noreply.github.com>
2025-06-05 12:49:44 -0500

    test: wrap App component rendering in act

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -24,9 +24,12 @@ import App from '../App';
 import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer, {act} from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(<App />);
-  expect(tree.toJSON()).toMatchSnapshot();
+it('renders correctly', async () => {
+  let tree;
+  await act(async () => {
+    tree = renderer.create(<App />);
+  });
+  expect(tree!.toJSON()).toMatchSnapshot();
 });
